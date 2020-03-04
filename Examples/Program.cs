d03b350a741dc38e441fd73479ff8ff50d1c4bfd
hayatoyu <f19870421@gmail.com>
2020-03-04 17:30:33 +0800

    20200304

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Examples
 {
@@ -6,28 +10,11 @@ namespace Examples
     {
         static void Main(string[] args)
         {
-            // 計算 97 是不是質數
-            // 質數定義：僅能被 1 和自己本身整除的正整數
-            // 質數找法：如果一個正整數在小於等於自己正平方根的範圍內找不到能整除它的數，那它就是質數
-            int target = 97;
-            int div = 2;
-            bool isPrime = true;
-
-            while(div * div <= target)
+            string[] students = { "Jessica", "Jenny", "Rebecca","Claire","Claire","Ashley" };            
+            for(int i = 0;i < students.length;i++)
             {
-                if(target % div == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-                div++;
-            }
 
-            if (isPrime)
-                Console.WriteLine($"{target} 是 質數");
-            else
-                Console.WriteLine($"{target} 不是 質數");
-            Console.ReadLine();
+            }
         }
     }
 }
