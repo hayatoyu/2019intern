e09a3ee3b6c0d39af6f20b8a5d9ff0fb86ec99fa
hayatoyu <f19870421@gmail.com>
2020-03-07 15:20:57 +0800

    20200307

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -10,9 +10,26 @@ namespace Examples
     {
         static void Main(string[] args)
         {
-            var names = new List<string> { "<name>", "Ana", "Felipe" };
-            foreach (var name in names)
-                Console.WriteLine($"Hello {name.ToUpper()}!");
+            List<string> employees = new List<string>();
+            employees.Add("Employee1");
+            employees.Add("Employee2");
+            employees.Add("Employee3");
+            Console.WriteLine($"公司的員工個數：{employees.Count}");
+
+            Console.WriteLine("公司新進了一個員工...");
+            employees.Add("Employee4");
+            Console.WriteLine($"公司目前的員工個數：{employees.Count}");
+
+            Console.WriteLine("Employee2離職了...");
+            employees.RemoveAt(1);
+            Console.WriteLine($"公司目前的員工個數：{employees.Count}");
+
+            Console.WriteLine("目前的員工依序為：");
+            for(int i = 0;i < employees.Count;i++)
+            {
+                Console.Write($"{employees[i]}\t");
+            }
+            Console.ReadLine();
         }
     }
 }
